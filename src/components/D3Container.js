1d572b4b762976bbb18dd66f5e0c4f314c42d92e
Horatiu Bota <horatiubota+github@gmail.com>
2019-12-06 13:03:54 +0200

    fixes bug with Gatsby useEffect not re-triggering

--- src/components/D3Container.js
+++ src/components/D3Container.js
@@ -1,160 +1,179 @@
-import React, { useEffect, useRef } from 'react'
-import ReactDOMServer from 'react-dom/server'
-import { select, mouse } from 'd3'
+import React, { useEffect, useRef } from "react";
+import ReactDOMServer from "react-dom/server";
+import { select, mouse } from "d3";
 
-import legend from '../utils/legend'
+import legend from "../utils/legend";
 
 /*
  Polygons and Points
 */
 
 const getColor = (scale, color, data) => {
-    return scale(data.map(item => item.value), color);
-}
+  return scale(
+    data.map(item => item.value),
+    color
+  );
+};
 
 const getPolygonSelection = (node, layer) => {
-    return select(node).select(`#${layer}`).selectAll('.polygonPath')
-}
+  return select(node)
+    .select(`#${layer}`)
+    .selectAll(".polygonPath");
+};
 
-const getPrimaryPolygonSelection = (node) => {
-    return getPolygonSelection(node, 'primary')
-}
+const getPrimaryPolygonSelection = node => {
+  return getPolygonSelection(node, "primary");
+};
 
-const getSecondaryPolygonSelection = (node) => {
-    return getPolygonSelection(node, 'secondary')
-}
+const getSecondaryPolygonSelection = node => {
+  return getPolygonSelection(node, "secondary");
+};
 
-const getPointSelection = (node) => {
-    return select(node).selectAll('.pointCircleGroup')
-}
+const getPointSelection = node => {
+  return select(node).selectAll(".pointCircleGroup");
+};
 
 const bindDataToSelection = (selection, data) => {
-    selection.datum(() => null).data(data, function(d) { return d ? d.id : this.id })
-}
+  selection
+    .datum(() => null)
+    .data(data, function(d) {
+      return d ? d.id : this.id;
+    });
+};
 
 const updateFillOnSelection = (selection, color) => {
-    selection.style('fill', d => (d !== undefined) ? color(d.value) : 'transparent')
-}
+  selection.style("fill", d =>
+    d !== undefined ? color(d.value) : "transparent"
+  );
+};
 
 /*
  Tooltip
 */
 
 const updateTooltip = (node, tooltip) => {
-    const [cursorX, cursorY] = mouse(node);
-    
-    select(node)
-    .select('#tooltipObject')
-    .attr('x', cursorX + 10)
-    .attr('y', cursorY + 10)
-    .select('#tooltipDiv')
-    .html(ReactDOMServer.renderToString(tooltip))    
-}
+  const [cursorX, cursorY] = mouse(node);
 
-const removeTooltip = (node) => {
-    select(node).select('#tooltipDiv').html('')
-}
-    
+  select(node)
+    .select("#tooltipObject")
+    .attr("x", cursorX + 10)
+    .attr("y", cursorY + 10)
+    .select("#tooltipDiv")
+    .html(ReactDOMServer.renderToString(tooltip));
+};
+
+const removeTooltip = node => {
+  select(node)
+    .select("#tooltipDiv")
+    .html("");
+};
+
 const attachTooltipToSelection = (node, selection, tooltip) => {
-    // `this` is the element on which the mouse event is triggered
-    // passed back to the tooltip function for customization options
-    selection.on('mousemove', function(d) { updateTooltip(node, tooltip(d, this)) })
-    select(node).on('mouseout', () => removeTooltip(node))
-}
+  // `this` is the element on which the mouse event is triggered
+  // passed back to the tooltip function for customization options
+  selection.on("mousemove", function(d) {
+    updateTooltip(node, tooltip(d, this));
+  });
+  select(node).on("mouseout", () => removeTooltip(node));
+};
 
 /*
  Legend
 */
 
 const onLegendMousemove = (node, classes, bounds, showSecondaryPaths) => {
-    const [lower, upper] = bounds;
-    
-    (showSecondaryPaths ? 
-        getSecondaryPolygonSelection(node) :
-        getPrimaryPolygonSelection(node)
-    ).classed(classes.highlightedPolygon, d => (d.value >= lower && d.value <= upper))
-}
+  const [lower, upper] = bounds;
+
+  (showSecondaryPaths
+    ? getSecondaryPolygonSelection(node)
+    : getPrimaryPolygonSelection(node)
+  ).classed(
+    classes.highlightedPolygon,
+    d => d.value >= lower && d.value <= upper
+  );
+};
 
 const onLegendMouseout = (node, classes, showSecondaryPaths) => {
-    (showSecondaryPaths ? 
-        getSecondaryPolygonSelection(node) :
-        getPrimaryPolygonSelection(node)
-    ).classed(classes.highlightedPolygon, false)
-}
+  (showSecondaryPaths
+    ? getSecondaryPolygonSelection(node)
+    : getPrimaryPolygonSelection(node)
+  ).classed(classes.highlightedPolygon, false);
+};
 
 const updateLegend = (node, color, props) => {
-    const { size, legend: legendProps, classes, showSecondaryPaths } = props
+  const { size, legend: legendProps, classes, showSecondaryPaths } = props;
 
-    legend({
-        ...legendProps,
-        node: node,
-        color: color,
-        tickFormat: ",.2r",
-        width: size.width,
-        marginLeft: size.width * 0.05,
-        marginRight: size.width * 0.05,
-        onMousemove: bounds => onLegendMousemove(node, classes, bounds, showSecondaryPaths),
-        onMouseout: () => onLegendMouseout(node, classes, showSecondaryPaths)
-    });
-}
+  legend({
+    ...legendProps,
+    node: node,
+    color: color,
+    tickFormat: ",.2r",
+    width: size.width,
+    marginLeft: size.width * 0.05,
+    marginRight: size.width * 0.05,
+    onMousemove: bounds =>
+      onLegendMousemove(node, classes, bounds, showSecondaryPaths),
+    onMouseout: () => onLegendMouseout(node, classes, showSecondaryPaths)
+  });
+};
 
 /*
  Click handler
 */
 
 const attachClickHandlerToSelection = (selection, f) => {
-    selection.on('click', function(d) { if (typeof f === 'function') { f(d) }});
-}
-  
+  selection.on("click", function(d) {
+    if (typeof f === "function") {
+      f(d);
+    }
+  });
+};
+
 export default function D3Container(props) {
+  const ref = useRef();
+  const { primaryMapData, secondaryMapData, pointMapData } = props;
+  const { size, scale, color, legend, tooltip } = props;
 
-    const ref = useRef();
-    const [mapSvg] = ref.current ? ref.current.children : []
+  useEffect(() => {
+    const [mapSvg] = ref.current ? ref.current.children : [];
 
-    const { primaryMapData, secondaryMapData, pointMapData } = props;
-    const { size, scale, color, legend, tooltip } = props;
+    if (mapSvg && primaryMapData && primaryMapData.length) {
+      const primaryPolygons = getPrimaryPolygonSelection(mapSvg);
+      const primaryColor = getColor(scale, color, primaryMapData);
 
-    useEffect(() => {
-        if (mapSvg && primaryMapData && primaryMapData.length) {            
-            const primaryPolygons = getPrimaryPolygonSelection(mapSvg)
-            const primaryColor = getColor(scale, color, primaryMapData)
+      bindDataToSelection(primaryPolygons, primaryMapData);
+      updateFillOnSelection(primaryPolygons, primaryColor);
+      attachTooltipToSelection(mapSvg, primaryPolygons, props.tooltip);
+      attachClickHandlerToSelection(primaryPolygons, props.onClick);
 
-            bindDataToSelection(primaryPolygons, primaryMapData)
-            updateFillOnSelection(primaryPolygons, primaryColor)
-            attachTooltipToSelection(mapSvg, primaryPolygons, props.tooltip)        
-            attachClickHandlerToSelection(primaryPolygons, props.onClick)
+      // secondary paths
+      if (secondaryMapData && secondaryMapData.length) {
+        const secondaryPolygons = getSecondaryPolygonSelection(mapSvg);
+        const secondaryColor = getColor(scale, color, secondaryMapData);
 
-            // secondary paths
-            if (secondaryMapData && secondaryMapData.length) {
-                const secondaryPolygons = getSecondaryPolygonSelection(mapSvg)
-                const secondaryColor = getColor(scale, color, secondaryMapData)
-                
-                bindDataToSelection(secondaryPolygons, secondaryMapData)
-                updateFillOnSelection(secondaryPolygons, secondaryColor)
-            }
+        bindDataToSelection(secondaryPolygons, secondaryMapData);
+        updateFillOnSelection(secondaryPolygons, secondaryColor);
+      }
 
-            // points
-            if (pointMapData && pointMapData.length) {
-                const points = getPointSelection(mapSvg)
+      // points
+      if (pointMapData && pointMapData.length) {
+        const points = getPointSelection(mapSvg);
 
-                bindDataToSelection(points, pointMapData)
-                attachTooltipToSelection(mapSvg, points, tooltip)
-            }
-        }
-    }, [primaryMapData, secondaryMapData, scale, color])
+        bindDataToSelection(points, pointMapData);
+        attachTooltipToSelection(mapSvg, points, tooltip);
+      }
+    }
+  }, [ref, primaryMapData, secondaryMapData, scale, color]);
 
-    // force legend re-draw on size, scale or color change
-    useEffect(() => {
-        const legendColor = secondaryMapData ? 
-            getColor(scale, color, secondaryMapData) :
-            getColor(scale, color, primaryMapData)
+  // force legend re-draw on size, scale or color change
+  useEffect(() => {
+    const [mapSvg] = ref.current ? ref.current.children : [];
+    const legendColor = secondaryMapData
+      ? getColor(scale, color, secondaryMapData)
+      : getColor(scale, color, primaryMapData);
 
-        mapSvg && updateLegend(mapSvg, legendColor, props)
-    }, [size, legend, scale, color])
+    mapSvg && updateLegend(mapSvg, legendColor, props);
+  }, [ref, size, legend, scale, color]);
 
-    return (
-        <div ref={ref}>
-            {props.children}
-        </div>
-    )
+  return <div ref={ref}>{props.children}</div>;
 }
