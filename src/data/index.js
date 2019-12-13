d23a09f251c8f1ecb8cf335d54b815971dc30c6d
Horatiu Bota <horatiubota+github@gmail.com>
2019-12-13 16:42:09 +0200

    changes geojson data to smaller topojson data

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,11 @@
-import counties from "./romania.counties.geo";
-import atus from "./romania.atus.geo";
-import cities from "./romania.cities.geo";
+import counties from "./romania.counties.topo"
+import atus from "./romania.atus.topo"
+import cities from "./romania.cities.topo"
+
+import { feature } from "topojson-client"
 
 export default {
-  counties: counties,
-  atus: atus,
-  cities: cities
-};
+  counties: feature(counties, counties.objects['romania.counties']),
+  atus: feature(atus, atus.objects['romania.atus.simple']),
+  cities: feature(cities, cities.objects['romania.cities'])
+}
