d321c19e14374cdf08dab88d091ab46b9921bddc
Horatiu Bota <52171232+horatiubota@users.noreply.github.com>
2019-12-18 13:52:37 +0200

    updates react-scripts dependency

--- example/src/components/Example.js
+++ example/src/components/Example.js
@@ -5,7 +5,6 @@ import { MapOfRomania, MapOfRomanianCounty } from "react-romania"
 import MapConfiguration from "./MapConfiguration"
 
 import Grid from "@material-ui/core/Grid"
-import Button from "@material-ui/core/Button"
 import { makeStyles } from "@material-ui/core/styles"
 import { scales, colors } from "../utils/mapConfigOptions"
 
