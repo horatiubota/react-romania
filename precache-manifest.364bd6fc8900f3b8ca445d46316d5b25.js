8cbeee3178bdc8238ae4629c27bf5a4beb858f27
Horatiu Bota <52171232+horatiubota@users.noreply.github.com>
2019-12-18 13:51:32 +0200

    Updates

---
+++ precache-manifest.364bd6fc8900f3b8ca445d46316d5b25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49da06e8d0463f9dfe1c",
+    "url": "/react-romania/static/css/main.fd13e40b.chunk.css"
+  },
+  {
+    "revision": "49da06e8d0463f9dfe1c",
+    "url": "/react-romania/static/js/main.12b7e75a.chunk.js"
+  },
+  {
+    "revision": "f8a79e0b9a0a8bb6cbc1",
+    "url": "/react-romania/static/js/runtime~main.921e3ef3.js"
+  },
+  {
+    "revision": "3235ff1a27ba4d2b3b97",
+    "url": "/react-romania/static/js/2.61fadb4c.chunk.js"
+  },
+  {
+    "revision": "a2b0c581862589939fd260ca447bb671",
+    "url": "/react-romania/index.html"
+  }
+];
