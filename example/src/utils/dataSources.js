c9417b80bdc8c584326558eeabbdf307dea99ce0
Horatiu Bota <52171232+horatiubota@users.noreply.github.com>
2020-11-26 12:49:26 +0200

    updates 107D data source

--- example/src/utils/dataSources.js
+++ example/src/utils/dataSources.js
@@ -1,5 +1,5 @@
 export default {
   countyPopulationDataUrl: "https://gist.githubusercontent.com/horatiubota/a8cc736f4c41106e272adc2287a90714/raw/3dcb97f0294f5595ee301617e9ad4fba6c7cca3e/numeromania_POP107A",
-  atuPopulationDataUrl: "https://gist.githubusercontent.com/horatiubota/77ce61d30388c3311d35701c2c2ae0b4/raw/dc5a94812ad225cd9ef52fafacc4f1aebb2c5b37/numeromania_POP107D",
+  atuPopulationDataUrl: "https://gist.githubusercontent.com/horatiubota/e210ad453b86135cefa460f673530a67/raw/d255e9ad825e30a63e9de456963d7d7161a8427f/numeromania_POP107D",
   cityPopulationDataUrl: "data/city_population_data.json",
 }
